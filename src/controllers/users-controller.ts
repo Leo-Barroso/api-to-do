c64383e336e14d702ec23b5c473b0b72c5bc369b
Leo <leobarrosoadm@gmail.com>
2025-01-30 00:35:25 -0300

    cadastra usuário no banco de dados

--- src/controllers/users-controller.ts
+++ src/controllers/users-controller.ts
@@ -1,8 +1,31 @@
 import { Request, Response, NextFunction } from "express";
+import { prisma } from "@/database/prisma";
+import { z } from "zod"
+import { hash } from "bcrypt"
+import { AppError } from "@/utils/AppError";
 
 class UsersController {
-    create(request: Request, response: Response) {
-        return response.json({ message: "Ok!"})
+    async create(request: Request, response: Response) {
+        const bodySchema = z.object({
+            name: z.string().trim().min(3),
+            email: z.string().email(),
+            password: z.string().min(6)
+        })
+        const { name, email, password} = bodySchema.parse(request.body)
+        const userWithSameEmail = await prisma.user.findFirst({ where: { email }})
+        if(userWithSameEmail) {
+            throw new AppError("User with same e-mail already exists")
+        }
+        const hashedPassword = await hash(password, 8)
+        const user = await prisma.user.create({
+            data: {
+                name,
+                email,
+                password: hashedPassword
+            }
+        })
+        const { password: _, ...userWithoutPassword} = user
+        return response.status(201).json(userWithoutPassword)
     }
 }
 
--- src/middlewares/error-handling.ts
+++ src/middlewares/error-handling.ts
@@ -1,7 +1,6 @@
 import { Request, Response, NextFunction } from "express";
 import { AppError } from "@/utils/AppError";
-import { z, ZodError } from "zod"
-
+import { ZodError } from "zod"
 export function errorHandling(
     error: any,
     request: Request,
