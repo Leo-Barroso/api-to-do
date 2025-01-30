bd0dbaf8514927e2fcb8b9ba0853178dcf3d4d1c
Leo <leobarrosoadm@gmail.com>
2025-01-29 23:34:03 -0300

    cria a tabela de usuários

---
+++ prisma/migrations/20250130021959_create_table_users/migration.sql
@@ -0,0 +1,21 @@
+-- CreateEnum
+CREATE TYPE "UserRole" AS ENUM ('admin', 'member');
+
+-- CreateEnum
+CREATE TYPE "TaskStatus" AS ENUM ('pending', 'in_progress', 'completed');
+
+-- CreateEnum
+CREATE TYPE "TaskPriority" AS ENUM ('high', 'medium', 'low');
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "role" "UserRole" NOT NULL DEFAULT 'member',
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3),
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
